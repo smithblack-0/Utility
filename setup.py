96c0d505a6ce008af89ef442e0ed56e30c25f68f
smithblack-0 <85000757+smithblack-0@users.noreply.github.com>
2022-02-10 14:14:07 -0800

    pip logic implimented

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 import setuptools
 
 setuptools.setup(
-    name='Utilities',
+    name='Utility',
     version = '0.0.1',
     author = "Chris O'Quinn",
     url = "https://github.com/smithblack-0/Utility",
-    packages = ['Layer']
+    packages = ['Layer'],
+    install_requires = ['torch', 'numpy']
 )
