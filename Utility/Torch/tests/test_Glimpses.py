ca08642fbee4f11a142cd54b4ce8a09d31c7ccaa
Chris O'Quinn <chrisoquinn.2@gmail.com>
2022-03-26 17:52:32 -0700

    Created tests for compress-decompress

--- Utility/Torch/tests/test_Glimpses.py
+++ Utility/Torch/tests/test_Glimpses.py
@@ -1,8 +1,7 @@
 import unittest
 import torch
 
-from Utility.Torch.Glimses import view
-from Utility.Torch.Glimses import local
+from Utility.Torch import Glimpses
 
 
 class testLocal(unittest.TestCase):
@@ -16,7 +15,7 @@ class testLocal(unittest.TestCase):
         kernel, stride, dilation = 1, 1, 1
         final = tensor.unsqueeze(-1)
 
-        test = local(tensor, kernel, stride, dilation)
+        test = Glimpses.local(tensor, kernel, stride, dilation)
         test = torch.all(test == final)
 
         self.assertTrue(test, "Logical failure: results did not match manual calculation")
@@ -30,7 +29,7 @@ class testLocal(unittest.TestCase):
         final = torch.tensor([[0 ,1] ,[1, 2], [2, 3], [3, 4], [4, 5]])
         kernel, stride, dilation = 2, 1, 1
 
-        test = local(tensor, kernel, stride, dilation)
+        test = Glimpses.local(tensor, kernel, stride, dilation)
         test = torch.all(test == final)
         self.assertTrue(test, "Logical failure: Kernels not equal")
     def testStriding(self):
@@ -43,7 +42,7 @@ class testLocal(unittest.TestCase):
         final = torch.tensor([[0], [2], [4]])
         kernel, stride, dilation = 1, 2, 1
 
-        test = local(tensor, kernel, stride, dilation)
+        test = Glimpses.local(tensor, kernel, stride, dilation)
         test = torch.all(test == final)
         self.assertTrue(test, "Logical failure: striding did not match")
     def testDilation(self):
@@ -61,9 +60,9 @@ class testLocal(unittest.TestCase):
         kernel2, stride2, dilation2 = 3, 1, 2
         kernel3, stride3, dilation3 = 2, 1, 3
 
-        test = local(tensor, kernel1, stride1, dilation1)
-        test2 = local(tensor, kernel2, stride2, dilation2)
-        test3 = local(tensor, kernel3, stride3, dilation3)
+        test = Glimpses.local(tensor, kernel1, stride1, dilation1)
+        test2 = Glimpses.local(tensor, kernel2, stride2, dilation2)
+        test3 = Glimpses.local(tensor, kernel3, stride3, dilation3)
 
         test = torch.all(final == test)
         test2 = torch.all(final2 == test2)
@@ -92,7 +91,68 @@ class testLocal(unittest.TestCase):
         final = torch.tensor(final)
 
         # test
-        test = local(tensor, kernel, striding, dilation)
+        test = Glimpses.local(tensor, kernel, striding, dilation)
         test = torch.all(final == test)
         self.assertTrue(test, "Logical failure: buffer issues")
 
+
+class testCompressDecompress(unittest.TestCase):
+    """
+    This is the test suite for the compress-decompress function
+    """
+    def testNoCompression(self):
+        """ Tests whether CompressDecompress works correctly when no compression occurs"""
+
+        #Setup test fixtures
+        tensor = torch.randn([10, 20, 30])
+        compress, decompress =  Glimpses.compress_decompress(0)
+
+        #perform test
+        compressed_tensor = compress(tensor)
+        expanded_tensor = decompress(compressed_tensor)
+        bool_result = torch.all(tensor == expanded_tensor)
+
+        shape_result = torch.equal(torch.tensor(compressed_tensor.shape), torch.tensor([1, 10, 20, 30]))
+        #Assert result
+        self.assertTrue(shape_result, "Compression occurred when none expected")
+        self.assertTrue(bool_result, "No compression case failed")
+    def testBasicCompression(self):
+        """ Tests whether CompressDecompress works with basic compression """
+
+        #Setup test fixtures
+        tensor = torch.randn([3, 4, 20, 230,4])
+        compress, decompress = Glimpses.compress_decompress(-2)
+
+        #Perform test
+        compressed_shape = torch.tensor([3*4*20, 230, 4])
+
+        compressed_tensor = compress(tensor)
+        decompressed_tensor = decompress(compressed_tensor)
+
+        same_bool = torch.equal(decompressed_tensor, tensor)
+        shape_bool = torch.equal(compressed_shape, torch.tensor(compressed_tensor.shape))
+
+        #Assert results
+
+        self.assertTrue(same_bool, "Basic compression failed - not the same")
+        self.assertTrue(shape_bool, "Basic compression failed - compressed shape not correct")
+    def testAsymmetricCompression(self):
+        """ Tests whether CompressDecompress can handle changing dimensions """
+
+        #Setup test fixtures
+        tensor = torch.randn([10, 20, 3, 5])
+        compress, decompress = Glimpses.compress_decompress(-2)
+
+        #Run compressions
+
+        compressed_a = compress(tensor)
+        compressed_b = compress(tensor)
+
+        #Modify dimensions
+        compressed_a = compressed_a[..., 0]
+        compressed_b = torch.stack([compressed_b, compressed_b], dim=-1)
+
+        #Run decompressions
+
+        decompressed_b = decompress(compressed_b)
+        decompressed_a = decompress(compressed_a)
