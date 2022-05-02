19b72f0fa2195ba5559748c190fda57f136412ae
Chris O'Quinn <chrisoquinn.2@gmail.com>
2022-05-01 17:35:22 -0700

    Began developing the stateful banded transformer.

---
+++ Utility/Torch/Models/BandedCompositeTransformer.py
@@ -0,0 +1,17 @@
+
+
+import torch
+from torch import nn
+
+
+
+class Encoder():
+    """ Encodes an incoming command or document"""
+    pass
+
+class State():
+    """Holds and updates the state document backend as questions are made"""
+    pass
+
+class Decoder():
+    """Decodes a response from the provided state"""
